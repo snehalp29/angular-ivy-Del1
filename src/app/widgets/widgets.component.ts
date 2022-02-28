4b137cef42d67ea83c6b053b83c675029df68463
snehalp29 <snehalpatkar@gmail.com>
2022-02-28 15:58:59 -0500

    added service

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -8,24 +8,7 @@ import { Widget } from '../app-interfaces';
 })
 export class WidgetsComponent implements OnInit {
   widgets$: Widget[] = [
-    {
-      id: '1',
-      title: 'title_1',
-      description: 'desc_1',
-      email: 'email_1@p.com',
-    },
-    {
-      id: '2',
-      title: 'title_2',
-      description: 'desc_2',
-      email: 'email_1@p.com',
-    },
-    {
-      id: '3',
-      title: 'title_3',
-      description: 'desc_3',
-      email: 'email_1@p.com',
-    },
+    
   ];
 
   selectedWidget$: Widget;
---
+++ src/app/widgets/widgets.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Widget } from '../app-interfaces';
+
+@Injectable({ providedIn: 'root' })
+export class WidgetsService {
+  widgets: Widget[] = [
+    {
+      id: '1',
+      title: 'title_1',
+      description: 'desc_1',
+      email: 'email_1@p.com',
+    },
+    {
+      id: '2',
+      title: 'title_2',
+      description: 'desc_2',
+      email: 'email_1@p.com',
+    },
+    {
+      id: '3',
+      title: 'title_3',
+      description: 'desc_3',
+      email: 'email_1@p.com',
+    },
+  ];
+
+  constructor() {}
+
+  findAll() {
+    return of(this.widgets);
+  }
+
+  findOne(id) {
+    return of(this.widgets.filter((w) => w.id === id));
+  }
+
+  create(widget: Widget) {
+    if (this.widgets.find((w) => w.email === widget.email)) {
+      throw 'email exists';
+    } else {
+      this.widgets.push(widget);
+    }
+  }
+
+  update(widget: Widget, id: string) {
+    return of(this.widgets.map((w) => (w.id === id ? this.widgets : w)));
+  }
+}
