229cbc076ff7709393c0274262802a0fe2e6ba64
snehalp29 <snehalpatkar@gmail.com>
2022-02-28 08:14:30 -0500

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, WidgetsComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    WidgetsComponent,
+    WidgetsListComponent,
+    WidgetDetailsComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
